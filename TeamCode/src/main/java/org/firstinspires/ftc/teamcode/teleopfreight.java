9520081a3bd3d0f35cf9a47fd0e0b1a995f68144
Aarav188 <83687013+Aarav188@users.noreply.github.com>
2021-10-18 10:21:10 -0700

    added comments, basic code for arm in teleop, need to add encoder values. 10/18/21 10:20 PM

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopfreight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopfreight.java
@@ -11,11 +11,23 @@ import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+
 @TeleOp(name="freight", group="TestBot")
 
 public class teleopfreight extends OpMode {
     HardwareTestbot robot = new HardwareTestbot();
-//encoder 138-140
+
+    PController pController = new PController(0.03);
+
+
+
+    double x1 = 0; // left
+    double y1 = 0; // right
+    double fortyFiveInRads = -Math.PI/4;
+    double cosine45 = Math.cos(fortyFiveInRads);
+    double sine45 = Math.sin(fortyFiveInRads);
+    double x2 = 0;
+    double y2 = 0;
 
     @Override
     public void init() {
@@ -47,28 +59,46 @@ public class teleopfreight extends OpMode {
      */
     @Override
     public void loop() {
-        double left;
-        double right;
 
-        // // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
-        left = -gamepad1.left_stick_y;
-        right = -gamepad1.right_stick_y;
+        //pid code
+        int encoderDegreesToAttain = 20000; //filler value
+        double minPower = 0.01;
+        double maxPower = 0.5;
+        pController.setInputRange(50, 500);
+        pController.setSetPoint(encoderDegreesToAttain);
+        pController.setOutputRange(minPower, maxPower);
 
-        robot.leftFront.setPower(left);
-        robot.leftBack.setPower(left);
-        robot.rightFront.setPower(right);
-        robot.rightBack.setPower(right);
+        double spin = gamepad1.right_stick_x;
 
-        //Strafe Left
-        if(gamepad1.left_bumper){
-            robot.leftFront.setPower(-robot.ONE);
-            robot.leftBack.setPower(robot.ONE);
-            robot.rightFront.setPower(robot.ONE);
-            robot.rightBack.setPower(-robot.ONE);
+        if(Math.abs(spin)>0.1){
+
+            robot.leftFront.setPower(spin);
+            robot.leftBack.setPower(spin);
+            robot.rightFront.setPower(-spin);
+            robot.rightBack.setPower(-spin);
+
+        } else {
+
+            y1 = -gamepad1.left_stick_y;
+            x1 =  gamepad1.left_stick_x;
+
+            //need to rotate 45 degrees
+            y2 = y1*cosine45 + x1*sine45;
+            x2 = x1*cosine45 - y1*sine45;
+
+            robot.leftFront.setPower(x2);
+            robot.leftBack.setPower(y2);
+            robot.rightFront.setPower(y2);
+            robot.rightBack.setPower(x2);
         }
 
 
-        if(gamepad1.a){
+        telemetry.addData("x1", "%.2f", x1);
+        telemetry.addData("y1", "%.2f", y1);
+        telemetry.addData("x2", "%.2f", x2);
+        telemetry.addData("y2", "%.2f", y2);
+
+        if(gamepad2.a){
             robot.carousel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             robot.carousel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             robot.carousel.setPower(0.3);
@@ -76,19 +106,84 @@ public class teleopfreight extends OpMode {
 
 
         }
+        //arm code using PID
+        if(gamepad2.b){
 
-        if(gamepad1.b){
-            robot.arm.setPower(0.3);
+            //move arm to low level, need to add encoder value
+            while (encoderDegreesToAttain != robot.arm.getCurrentPosition()) {
+                telemetry.addData("encoder position", robot.arm.getCurrentPosition());
+                telemetry.addData("power", pController.getComputedOutput(robot.arm.getCurrentPosition()));
+                telemetry.update();
+                if (robot.arm.getCurrentPosition() < encoderDegreesToAttain) {
+                    robot.arm.setPower(minPower + pController.getComputedOutput(robot.arm.getCurrentPosition()));
+                } else {
+                    robot.arm.setPower(minPower - pController.getComputedOutput(robot.arm.getCurrentPosition()));
+                }
+
+                if(robot.arm.getCurrentPosition()>=34500 || (robot.arm.getCurrentPosition()<=35500)){
+                    robot.arm.setPower(0);
+                }
+            }
+        }
+
+        if(gamepad2.x){
+            //move arm to mid level, need to add encoder value
+            while (encoderDegreesToAttain != robot.arm.getCurrentPosition()) {
+                telemetry.addData("encoder position", robot.arm.getCurrentPosition());
+                telemetry.addData("power", pController.getComputedOutput(robot.arm.getCurrentPosition()));
+                telemetry.update();
+                if (robot.arm.getCurrentPosition() < encoderDegreesToAttain) {
+                    robot.arm.setPower(minPower + pController.getComputedOutput(robot.arm.getCurrentPosition()));
+                } else {
+                    robot.arm.setPower(minPower - pController.getComputedOutput(robot.arm.getCurrentPosition()));
+                }
+
+                if(robot.arm.getCurrentPosition()>=34500 || (robot.arm.getCurrentPosition()<=35500)){
+                    robot.arm.setPower(0);
+                }
+            }
+        }
+
+        if(gamepad2.y){
+            //move arm to high level, need to set encoder value
+            while (encoderDegreesToAttain != robot.arm.getCurrentPosition()) {
+                telemetry.addData("encoder position", robot.arm.getCurrentPosition());
+                telemetry.addData("power", pController.getComputedOutput(robot.arm.getCurrentPosition()));
+                telemetry.update();
+                if (robot.arm.getCurrentPosition() < encoderDegreesToAttain) {
+                    robot.arm.setPower(minPower + pController.getComputedOutput(robot.arm.getCurrentPosition()));
+                } else {
+                    robot.arm.setPower(minPower - pController.getComputedOutput(robot.arm.getCurrentPosition()));
+                }
+
+                if(robot.arm.getCurrentPosition()>=34500 || (robot.arm.getCurrentPosition()<=35500)){
+                    robot.arm.setPower(0);
+                }
+            }
+        }
+
+        if(gamepad2.dpad_up){
+            //move arm to top, need to set encoder value
+            while (encoderDegreesToAttain != robot.arm.getCurrentPosition()) {
+                telemetry.addData("encoder position", robot.arm.getCurrentPosition());
+                telemetry.addData("power", pController.getComputedOutput(robot.arm.getCurrentPosition()));
+                telemetry.update();
+                if (robot.arm.getCurrentPosition() < encoderDegreesToAttain) {
+                    robot.arm.setPower(minPower + pController.getComputedOutput(robot.arm.getCurrentPosition()));
+                } else {
+                    robot.arm.setPower(minPower - pController.getComputedOutput(robot.arm.getCurrentPosition()));
+                }
+
+                if(robot.arm.getCurrentPosition()>=34500 || (robot.arm.getCurrentPosition()<=35500)){
+                    robot.arm.setPower(0);
+                }
+            }
         }
 
 
-        //Strafe Right
-        if(gamepad1.right_bumper){
-            robot.leftFront.setPower(robot.ONE);
-            robot.leftBack.setPower(-robot.ONE);
-            robot.rightFront.setPower(-robot.ONE);
-            robot.rightBack.setPower(robot.ONE);
-        }
+
+
+
 
 
 
@@ -96,9 +191,7 @@ public class teleopfreight extends OpMode {
         double down;
 
 
-        // Send telemetry message to signify robot running;
-        telemetry.addData("left",  "%.2f", left);
-        telemetry.addData("right", "%.2f", right);
+
 
     }
 
