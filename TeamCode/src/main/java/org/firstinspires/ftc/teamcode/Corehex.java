51a58e128314b0cb88e1dcb00ca68a3d2248d332
Aarav188 <83687013+Aarav188@users.noreply.github.com>
2021-10-22 20:57:23 -0700

    corehex updated

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Corehex.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+
+
+
+/*
+Logic for the encoder incremental movement for Sat.
+
+
+ */
+
+
+@TeleOp(name="linear actuator", group="TestBot")
+//@Disabled
+public class Corehex extends LinearOpMode {
+
+
+
+    HardwareTestbot         robot   = new HardwareTestbot();   // Use a Pushbot's hardware
+    ElapsedTime runtime = new ElapsedTime();
+
+    public DcMotor corehex;
+
+
+
+
+    @Override
+    public void runOpMode(){
+        robot.init(hardwareMap);
+        corehex = hardwareMap.dcMotor.get("corehex");
+        int encoderDegreesToAttain = 1000;
+        double minPower = 0.01;
+        double maxPower = 0.5;
+        PController pController = new PController(0.03);
+        pController.setInputRange(50, 500);
+        pController.setSetPoint(encoderDegreesToAttain);
+        pController.setOutputRange(minPower, maxPower);
+
+
+        waitForStart();
+
+
+
+
+    }
+
+    public void runhex(double rotations, double power ){
+        double rotation_degree = 288;
+
+        int degrees = (int)rotation_degree*rotations;
+        corehex.setTargetPosition(degrees);
+
+
+
+    }
+}
+
+
+
+
