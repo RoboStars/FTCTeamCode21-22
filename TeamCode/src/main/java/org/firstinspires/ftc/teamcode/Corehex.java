066ffd98fb467544159806026a11f1da1c856d7f
Rishabh <55954960+Rishabh942@users.noreply.github.com>
2021-11-05 23:04:14 -0700

    teleop freight fix and simple tele addition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Corehex.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Corehex.java
@@ -1,39 +1,76 @@
 package org.firstinspires.ftc.teamcode;
 
-
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp
+
+
+
+
+/*
+Logic for the encoder incremental movement for corehex
+
+
+ */
+
+
+@TeleOp(name="corehex", group="TestBot")
+//@Disabled
 public class Corehex extends LinearOpMode {
-    DcMotorEx motor;
+
+
+
+    HardwareTestbot         robot   = new HardwareTestbot();   // Use a Pushbot's hardware
+    ElapsedTime runtime = new ElapsedTime();
+
+    public DcMotor corehex;
+
+
+
 
     @Override
-    public void runOpMode() {
-        motor = hardwareMap.get(DcMotorEx.class, "corehex");
+    public void runOpMode(){
+        robot.init(hardwareMap);
+        corehex = hardwareMap.dcMotor.get("corehex");
+        int encoderDegreesToAttain = 1000;
+        double minPower = 0.01;
+        double maxPower = 0.5;
+        PController pController = new PController(0.03);
+        pController.setInputRange(50, 500);
+        pController.setSetPoint(encoderDegreesToAttain);
+        pController.setOutputRange(minPower, maxPower);
 
-        // Reset the encoder during initialization
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         waitForStart();
 
-        // Set the motor's target position to 300 ticks
-        motor.setTargetPosition(-300);
+        runhex(1, 0.4);
+        telemetry.addData("encoder position", corehex.getCurrentPosition());
+        telemetry.update();
+        telemetry.setAutoClear(false);
 
-        // Switch to RUN_TO_POSITION mode
-        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        // Start the motor moving by setting the max velocity to 200 ticks per second
-        motor.setVelocity(-200);
 
-        // While the Op Mode is running, show the motor's status via telemetry
-        while (opModeIsActive()) {
-            telemetry.addData("velocity", motor.getVelocity());
-            telemetry.addData("position", motor.getCurrentPosition());
-            telemetry.addData("is at target", !motor.isBusy());
-            telemetry.update();
-        }
+    }
+
+    public void runhex(double rotations, double power ){
+        double rotation_degree = 288;
+
+        int degrees = (int)(rotation_degree*rotations);
+        corehex.setTargetPosition(degrees);
+        corehex.setPower(power);
+        corehex.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        corehex.setPower(0);
+
+
+
+
     }
 }
+
+
+
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBotSimpleTeleopRight.java
@@ -0,0 +1,124 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.robot.Robot;
+import java.lang.annotation.Target;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+
+@TeleOp(name="TestBot: SimpleTeleopRight", group="TestBot")
+public class TestBotSimpleTeleopRight extends OpMode {
+    //HardwareTestbot robot = new HardwareTestbot();
+    
+    public DcMotor  rightFront = null;
+    public DcMotor rightBack = null;
+    
+    
+    
+    double armPosition = 0.00;
+    final double ARM_SPEED = 0.002;
+    
+    
+    
+ @Override
+    public void init() {
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+       // robot.init(hardwareMap);
+       
+       
+        rightFront = hardwareMap.dcMotor.get("frontR");
+        rightBack = hardwareMap.dcMotor.get("backR");
+        
+        
+        
+        rightFront.setDirection(DcMotor.Direction.FORWARD); 
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+        
+       
+       
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        double left;
+        double right;
+
+        // // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+         left = -gamepad1.left_stick_y;
+         right = -gamepad1.right_stick_y;
+         
+        //  double Target = 0;
+        //  robot.rightBack.setTargetPosition((int)Target);
+        //  robot.rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+         rightFront.setPower(left);
+         rightBack.setPower(right);
+        // robot.rightDrive.setPower(right);
+
+        // // Use gamepad left & right Bumpers to open and close the claw
+        // if (gamepad1.right_bumper)
+        //     clawOffset += CLAW_SPEED;
+        // else if (gamepad1.left_bumper)
+        //     clawOffset -= CLAW_SPEED;
+
+        // // Move both servos to new position.  Assume servos are mirror image of each other.
+        // clawOffset = Range.clip(clawOffset, -0.5, 0.5);
+        // robot.leftClaw.setPosition(robot.MID_SERVO + clawOffset);
+        // robot.rightClaw.setPosition(robot.MID_SERVO - clawOffset);
+
+        // Use gamepad buttons to move the arm up (Y) and down (A)
+        // if (gamepad1.y)
+        //     armPosition += ARM_SPEED;
+           
+        // if (gamepad1.a)
+        //     armPosition -= ARM_SPEED;
+        
+        // armPosition = Range.clip(armPosition, robot.ARM_MIN_RANGE, robot.ARM_MAX_RANGE);    
+     
+
+        // // Send telemetry message to signify robot running;
+        // telemetry.addData("arm",  "%.2f", armPosition);
+        // telemetry.addData("left",  "%.2f", left);
+        // telemetry.addData("right", "%.2f", right);
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+}
+ 
+
+
